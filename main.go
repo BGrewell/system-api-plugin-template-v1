f30838e96e62b7a5acba5c348709c54846207fc6
Ben Grewell <BGrewell@gmail.com>
2022-02-01 09:49:34 -0800

    Create main.go
    add template example

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+  "github.com/BGrewell/system-api/plugin"
+  "github.com/gin-gonic/gin"
+)
+
+func Load() plugin.Plugin {
+  return TemplatePlugin{}
+}
+
+type TemplatePlugin struct {
+  Message string
+}
+
+func (p *TemplatePlugin) Register(config map[string]interface{}, r *gin.RouterGroup) error {
+  // Load configuration if it exists
+  p.Message = "Template Plugin"
+  if config != nil {
+    if msg, ok := config["message"]; ok {
+      p.Message = msg.(string)
+    }
+  }
+  
+  // Register routes
+  r.GET("/", p.HomeHandler)
+}
+
+func (p *TemplatePlugin) Name() string {
+  return "Template"
+}
+
+func (p *TemplatePlugin) HomeHandler(c *gin.Context) {
+  c.JSON(200, gin.H{
+    "message": p.Message,
+  })
+}
